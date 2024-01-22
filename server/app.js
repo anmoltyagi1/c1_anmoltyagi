e86ce86c228979ccb3d370d22e32c6326d6105de
Anmol Tyagi <anmolty2@gmail.com>
2024-01-22 01:09:24 -0500

    changed app.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const api = require("./api");
 const app = express();
-
+const cors = require("cors");
 // Middleware to parse JSON in the request body
 app.use(express.json());
 
+app.use(cors());
+
 app.use("/", api);
+
+app.listen(3001, () => {
+  console.log(`Server listening on 3001`);
+});
 
 // module.exports.handler = serverless(app);
 module.exports = app;
